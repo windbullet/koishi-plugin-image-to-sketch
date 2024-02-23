4de674a1d04cfff2a1f5fcc23599db7e7ea6a13e
windbullet <2536810643@qq.com>
2024-02-23 11:07:06 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-image-to-sketch?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-image-to-sketch)
 
 将图片变成类似于素描的画
+使用的API：https://api.xingzhige.com/
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 
 export const name = 'image-to-sketch'
 
@@ -7,5 +7,16 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('sketch <img:image>')
+    .action(async ({ session }, img) => {
+      let buffer = Buffer.from(await ctx.http.get(img.src, { responseType: "arraybuffer" }))
+      let config = {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data: `url=${encodeURIComponent(buffer.toString('base64'))}`
+      }
+      let res = await ctx.http('post', 'https://api.xingzhige.com/API/xian/', config)
+      return h.image(res.data, res.headers.get('content-type'))
+    })
 }
